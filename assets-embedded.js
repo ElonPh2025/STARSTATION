309bc353f3d186b4c3ba8a498a6f3ddbd6a801d6
theG2026 <gbbaria02@gmail.com>
2026-08-10 11:33:14 +0800

    feat: STARSTATION app shell (HTML, embeds loader, Pages workflow)

---
+++ assets-embedded.js
@@ -0,0 +1,6 @@
+import c0 from './assets-embedded-0.js';
+import c1 from './assets-embedded-1.js';
+import c2 from './assets-embedded-2.js';
+import c3 from './assets-embedded-3.js';
+import c4 from './assets-embedded-4.js';
+export const EMBEDDED_ASSETS = Object.assign({}, c0, c1, c2, c3, c4);
