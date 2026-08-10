c9c91b204f479917ec79147cbf143b3a7d379160
theG2026 <gbbaria02@gmail.com>
2026-08-10 11:41:04 +0800

    feat: add embedded asset modules (small) and netlify config

--- assets-embedded.js
+++ assets-embedded.js
@@ -1,6 +1,22 @@
-import c0 from './assets-embedded-0.js';
-import c1 from './assets-embedded-1.js';
-import c2 from './assets-embedded-2.js';
-import c3 from './assets-embedded-3.js';
-import c4 from './assets-embedded-4.js';
-export const EMBEDDED_ASSETS = Object.assign({}, c0, c1, c2, c3, c4);
+import u0 from './asset_models_optimized_starship_top_glb.js';
+import u1 from './asset_models_optimized_engine_big_glb.js';
+import u2 from './asset_models_lights_Light_01_glb.js';
+import u3 from './asset_models_lights_Light_02_glb.js';
+import u4 from './asset_models_lights_Light_03_glb.js';
+import u5 from './asset_textures_starship_heat_shield_optimized_Base_color_2k_webp.js';
+import u6 from './asset_textures_starship_heat_shield_optimized_Roughness_2k_webp.js';
+import u7 from './asset_textures_perlinnoise_webp.js';
+import u8 from './asset_engines_starship_json.js';
+import u9 from './asset_manifest_json.js';
+export const EMBEDDED_ASSETS = {
+  "models/optimized/starship_top.glb": u0,
+  "models/optimized/engine_big.glb": u1,
+  "models/lights/Light_01.glb": u2,
+  "models/lights/Light_02.glb": u3,
+  "models/lights/Light_03.glb": u4,
+  "textures/starship/heat_shield/optimized/Base_color_2k.webp": u5,
+  "textures/starship/heat_shield/optimized/Roughness_2k.webp": u6,
+  "textures/perlinnoise.webp": u7,
+  "engines_starship.json": u8,
+  "manifest.json": u9,
+};
